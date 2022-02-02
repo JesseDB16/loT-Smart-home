eafb30aa21a737f69931f80f03e6f418528bfd44
JesseDB16 <89937249+JesseDB16@users.noreply.github.com>
2022-02-02 11:05:23 +0100

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,13 @@
+
+# WiFi credentials
+WLAN_SSID = "IOT-IN"
+WLAN_PASS = "93880195750058952287"
+
+# broker configuration
+CLIENTID="esp32 - 12"                 # esp32 - group number!
+BROKER="10.0.0.1"                   # MQTT broker IP address
+PORT=1883                           # MQTT port
+
+# MQTT credentials
+HA_USER="homeassistant"
+HA_PASS="Im0ohbei1eetaem1achaem1ophei7EeSheisie8einebuage2hee0oukootouw2i"
